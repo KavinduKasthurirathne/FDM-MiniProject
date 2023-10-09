6c261ed5c74d0ec60433bf2ce770a8811900c48f
surith <surith1234@gmail.com>
2023-10-09 10:46:32 +0530

    ui edit

--- LoanApp.py
+++ LoanApp.py
@@ -40,7 +40,7 @@ def main():
             <tr><td> luxury_asset_value</td><td>Value of the luxury asset of the applicant</td></tr>
             <tr><td> bank_assets_value</td><td>Value of the bank asset of the applicant</td></tr>
             </table><br>""", unsafe_allow_html=True)
-    st.write("blue[Click the left side bar to insert information]")
+    st.write(':blue[_Click the left side bar to insert information_]')
     st.write("Please click the button below to see the loan status prediction")
     # Sidebar with user inputs
 
